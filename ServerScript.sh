26c7878201231a1003576b6fce540e5a9a4e65b8
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 16:10:17 +1000

    fixed paths issues

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,6 +1,6 @@
 #!/bin/dash
 echo "git pull"
 git pull
-cp ~/home/sam/docker/docker-compose.yml ~/home/sam/docker/docker-compose.bak
-cp docker-composet1.yml ~/home/sam/docker/docker-compose.yml
+cp /home/sam/docker/docker-compose.yml /home/sam/docker/docker-compose.bak
+cp docker-composet1.yml /home/sam/docker/docker-compose.yml
 echo
