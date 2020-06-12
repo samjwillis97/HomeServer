7e6d95cbb7231e007cfb75ab33c0812e7c6be323
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 16:06:57 +1000

    add copying of docker compose files

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,4 +1,6 @@
 #!/bin/dash
 echo "git pull"
 git pull
+cp ~/home/sam/docker/docker-compose.yml ~/home/sam/docker/docker-compose.bak
+cp docker-composet1.yml ~/home/sam/docker/docker-compose.yml
 echo
