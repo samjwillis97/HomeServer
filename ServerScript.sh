b7a76daffbb8863a7e42d62b62b64742f125a1ea
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 16:19:34 +1000

    added more print statements, and a docker compose

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,6 +1,9 @@
 #!/bin/dash
-echo "git pull"
+echo "Git pull"
 git pull
+echo "Backing up previous compose file"
 cp /home/sam/docker/docker-compose.yml /home/sam/docker/docker-compose.bak
+echo "Importing compose from repository"
 cp docker-composet1.yml /home/sam/docker/docker-compose.yml
-echo
+echo "Attempting docker recreate"
+docker-compose -f ~/docker/docker-compose.yml up -d
