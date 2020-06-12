a18b3e48a6f2c508ffab6fde807b2bbf8cea9251
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 19:42:18 +1000

    added a comment for future and colours to print

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-echo "Moving to github directory"
+# Ideally add a loop to check environment variables required are within the environment file
+
+CYAN='\033[1;36m'
+
+echo -e "${CYAN}Moving to github directory"
 pushd /home/sam/github/HomeServer
-echo "Git pull"
+echo -e "${CYAN}Git pull"
 git pull
-echo "Updating Script"
+echo -e "${CYAN}Updating Script"
 pushd +1
 cp /home/sam/github/HomeServer/ServerScript.sh /home/sam/ServerScript.sh
-echo "Moving to docker-compose directory"
+echo -e "${CYAN}Moving to docker-compose directory"
 pushd /home/sam/docker/
-echo "Backing up previous compose file"
+echo -e "${CYAN}Backing up previous compose file"
 cp /home/sam/docker/docker-compose.yml /home/sam/docker/docker-compose.bak
-echo "Importing compose from repository"
+echo -e "${CYAN}Importing compose from repository"
 cp /home/sam/github/HomeServer/docker-composet1.yml /home/sam/docker/docker-compose.yml
-echo "Attempting docker recreate"
+echo -e "${CYAN}Attempting docker recreate"
 docker-compose -f ~/docker/docker-compose.yml up -d
