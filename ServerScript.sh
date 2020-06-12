6ed3a34d4db5003f3aede0d2ca9dce62285a7dcd
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 19:24:09 +1000

    trying to run script from home

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,9 +1,16 @@
 #!/bin/dash
+echo "Moving to github directory"
+pushd /home/sam/github/HomeServer
 echo "Git pull"
 git pull
+echo "Updating Script"
+pushd -1
+cp /home/sam/github/Homeserver/ServerScript.sh ServerScript.sh
+echo "Moving to docker-compose directory"
+pushd /home/sam/docker/
 echo "Backing up previous compose file"
 cp /home/sam/docker/docker-compose.yml /home/sam/docker/docker-compose.bak
 echo "Importing compose from repository"
-cp docker-composet1.yml /home/sam/docker/docker-compose.yml
+cp /home/sam/github/HomeServer/docker-composet1.yml /home/sam/docker/docker-compose.yml
 echo "Attempting docker recreate"
 docker-compose -f ~/docker/docker-compose.yml up -d
