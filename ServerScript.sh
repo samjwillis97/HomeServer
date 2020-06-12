c4f4a069a89db0af6c9a5ad516587b0a90380eb3
Samuel Willis <sam.willis@verico.com.au>
2020-06-12 19:54:50 +1000

    removed error function

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
-# Ideally add a loop to check environment variables required are within the environment file
+# Ideally add a loop to  environment variables required are within the environment file
 #
 RED='\033[0;31m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-function checkError {
-if [ $? -eq 0 ]
-then
-else
-    echo -e "${RED}ERROR${NC}"
-    echo ERROR
-fi
-}
-
 echo -e "${CYAN}Moving to github directory${NC}"
 pushd /home/sam/github/HomeServer
 echo -e "${CYAN}Git pull${NC}"
 git pull
-checkError
 echo -e "${CYAN}Updating Script${NC}"
 pushd +1
 cp /home/sam/github/HomeServer/ServerScript.sh /home/sam/ServerScript.sh
-checkError
 echo -e "${CYAN}Moving to docker-compose directory${NC}"
 pushd /home/sam/docker/
 echo -e "${CYAN}Backing up previous compose file${NC}"
 cp /home/sam/docker/docker-compose.yml /home/sam/docker/docker-compose.bak
-checkError  
 echo -e "${CYAN}Importing compose from repository${NC}"
 cp /home/sam/github/HomeServer/docker-composet1.yml /home/sam/docker/docker-compose.yml
-checkError
 echo -e "${CYAN}Attempting docker recreate${NC}"
 docker-compose -f ~/docker/docker-compose.yml up -d
-checkError
